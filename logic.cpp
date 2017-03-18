30f7ab76657e3d347c576ac69637e6ccf24b4c99
Anna <anna@MacBook-Pro.local>
2017-03-18 00:17:04 +0000

    Commit

--- logic.cpp
+++ logic.cpp
@@ -75,7 +75,7 @@ void Logic::handleSortFilesBtn() {                                              
             // --------------------------------------------
 
             QString copyAttemptOutcome = copyFileToFolder(fileName, outputFolder);              // Attempt our copy and return a QString detailing outcome.
-            logWindow->print(copyAttemptOutcome);                                               // Then send the returned outcome string to log window.1
+            logWindow->print(copyAttemptOutcome);                                               // Then send the returned outcome string to log window.
 
         } else {
 
