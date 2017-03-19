a9f34366630d6bcf9bb5983e9019885ccf4a8728
Anna <anna@MacBook-Pro.local>
2017-03-19 03:25:13 +0000

    Commit ${var}

--- git.sh
+++ git.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
+
+if [ ! -f "./commitNo" ] ; then
+  var=0
+
+else
+  value=`cat ./commitNo`
+fi
+
+value=`expr ${var} + 1`
+
 cd /Users/anna/PdfSizeSorter
-git add -u && git commit -m "Commit" && git push
+git add -u && git commit -m 'Commit ${var}' && git push
+
+echo "${var}" > ./commitNo
+
 sleep 2
+
+
+
